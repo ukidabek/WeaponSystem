a86ec0407b6138b9aa842a0bfbfc96962a38367d
Ukidabek <ukidabek@gmail.com>
2018-04-02 22:02:16 +0200

    - transform Weapon handling logic added.

---
+++ BaseTransformWeaponLogic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WeaponSystem
+{
+	public abstract class BaseTransformWeaponLogic : BaseWeaponLogic 
+	{
+		[SerializeField] protected Transform transform = null;
+
+		public void SetTransform(Object transform)
+		{
+			if(transform is Transform)
+			{
+				this.transform = transform as Transform;
+			}
+		}	
+	}
+}
