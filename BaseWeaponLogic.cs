46d1983026d4ef810dd6c93b84e7bdb2aa06a63f
Ukidabek <ukidabek@gmail.com>
2018-06-03 10:56:42 +0200

    - Refactor

--- BaseWeaponLogic.cs
+++ BaseWeaponLogic.cs
@@ -6,6 +6,6 @@ namespace WeaponSystem
 {
 	public abstract class BaseWeaponLogic : MonoBehaviour 
 	{
-		public abstract void Perform();
+		public abstract void Perform(params object[] data);
 	}
 }
