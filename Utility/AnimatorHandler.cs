cdfa3dab0121c939b0a682aec2399e42b731a5f0
Ukidabek <ukidabek@gmail.com>
2018-06-25 20:26:40 +0200

    - Weapon system update.

--- Utility/AnimatorHandler.cs
+++ Utility/AnimatorHandler.cs
@@ -9,5 +9,63 @@ namespace WeaponSystem.Utility
     public class AnimatorHandler
     {
         public Animator Animator = null;
+        [SerializeField] private string _name = string.Empty;
+        [SerializeField] private int _nameHash = 0;
+
+        private bool _oldBoolValue;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                _nameHash = Animator.StringToHash(value);
+            }
+        }
+
+        public void Set()
+        {
+            Set(Animator);
+        }
+
+        public void Set(float value)
+        {
+            Set(Animator, value);
+        }
+
+        public void Set(float value, float dampTime)
+        {
+            Set(Animator, value, dampTime);
+        }
+
+        public void Set(bool value)
+        {
+            Set(Animator, value);
+        }
+
+        public void Set(Animator animator)
+        {
+            animator.SetTrigger(_nameHash);
+        }
+
+        public void Set(Animator animator, float value)
+        {
+            animator.SetFloat(_nameHash, value);
+        }
+
+        public void Set(Animator animator, float value, float dampTime)
+        {
+            animator.SetFloat(_nameHash, value, dampTime, Time.deltaTime);
+        }
+
+        public void Set(Animator animator, bool value)
+        {
+            if (_oldBoolValue != value)
+            {
+                animator.SetBool(_nameHash, value);
+                _oldBoolValue = value;
+            }
+        }
     }
 }
---
+++ Utility/Editor/AnimatorHandlerPropertyDrawer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace WeaponSystem.Utility
+{
+    [CustomPropertyDrawer(typeof(AnimatorHandler))]
+    public class AnimatorHandlerPropertyDrawer : PropertyDrawer
+    {
+        private List<string> parameters = new List<string>();
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUIUtility.singleLineHeight * 2;
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var currentPosition = position;
+            currentPosition.height = EditorGUIUtility.singleLineHeight;
+            SerializedProperty animatorProperty = property.FindPropertyRelative("Animator");
+            EditorGUI.PropertyField(currentPosition, animatorProperty, new GUIContent("Animator"));
+            currentPosition.y += EditorGUIUtility.singleLineHeight;
+
+            Animator animatorController = animatorProperty.objectReferenceValue as Animator;
+            if (animatorController != null)
+            {
+                if (animatorController.parameters.Length != parameters.Count)
+                {
+                    parameters.Clear();
+                    foreach (var item in animatorController.parameters)
+                    {
+                        parameters.Add(item.name);
+                    }
+                }
+            }
+
+            EditorGUI.Popup(currentPosition, 0, parameters.ToArray());
+        }
+    }
+}
